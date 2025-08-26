900de980f24d8dac12e849be1820cb250b06886e
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2025-08-25 20:09:55 -0500

    initial commit

---
+++ src/Persona.java
@@ -0,0 +1,34 @@
+public class Persona {
+  private String nombre;
+  private double dinero;
+
+  public Persona(String nombre, double dinero) {
+    this.nombre = nombre;
+    this.dinero = dinero;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre() {
+    this.nombre = nombre;
+  }
+
+  public double getDinero() {
+    return dinero;
+  }
+
+  public void setDinero(double dinero) {
+    this.dinero = dinero;
+  }
+
+  public void recibirProducto(Producto producto) {
+    System.out.println("La persona " + this.nombre + " ha recibido el producto: ");
+  }
+
+  public void recibirCambio(double cambio) {
+    this.dinero += cambio;
+    System.out.println("La persona " + this.nombre + " ha recibido un cambio de: " + cambio);
+  }
+}
