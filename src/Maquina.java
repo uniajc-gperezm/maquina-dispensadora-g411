95561cad8a4e4165c79e4e34aab4a9f056ebf230
Sebastian Acuña <sebsg1806@gmail.com>
2025-08-25 20:38:23 -0500

    agrego atributos y metodos clase maquina

--- src/Maquina.java
+++ src/Maquina.java
@@ -1,3 +1,43 @@
+import java.util.Map;
+
 public class Maquina {
+
+private double dineroRecibido;
+private Map inventarioMap;
+
+public Maquina(double dineroRecibido, Map inventarioMap) {
+    this.dineroRecibido = dineroRecibido;
+    this.inventarioMap = inventarioMap;
+}
+
+public double getDineroRecibido() {
+    return dineroRecibido;
+}
+
+public void setDineroRecibido(double dineroRecibido) {
+    this.dineroRecibido = dineroRecibido;
+}
+
+public Map getInventarioMap() {
+    return inventarioMap;
+}
+
+public void setInventarioMap(Map inventarioMap) {
+    this.inventarioMap = inventarioMap;
+}
+
+public void cargarProducto (Producto producto) {
+
+}
+
+public void mostrarProducto (){
+
+}
+
+public void seleccionarProducto(Persona persona){
+
+}
+
+
 
 }
