add08c92f2fc83b7d58db1b1f42412896f8f5f36
Valeri-Solis1 <vasolis@estudiante.uniajc.edu.co>
2025-08-26 01:13:14 -0500

    Descripción de tu nuevo cambio

--- src/Producto.java
+++ src/Producto.java
@@ -1,6 +1,50 @@
 public class Producto {
- private String codigo;
- private String nombre;
- private int cantidad;
- private double precio;
+
+private String codigo;
+private String nombre;
+private int cantidad;
+private double precio;
+
+public Producto (String codigo, String nombre, int cantidad, double precio){
+    this.codigo = codigo;
+    this.nombre = nombre;
+    this.cantidad = cantidad;
+    this.precio = precio;
+}
+
+public String getCodigo(){
+    return codigo;
+}
+public void setCodigo(String codigo){
+    this.codigo = codigo;
+}
+public String getNombre(){
+    return nombre;
+}
+public void setNombre(String nombre){
+    this.nombre = nombre;
+}
+public int getCantidad(){
+    return cantidad;
+}
+public void setCantidad(int cantidad){
+    this.cantidad = cantidad;
+}
+public double getPrecio(){
+    return precio;
+}
+public void setPrecio(double precio){
+    this.precio = precio;
+}
+public void dispensar() {
+    if (cantidad > 0) {
+        cantidad--;
+        System.out.println("Producto dispensado: " + nombre + ". Cantidad restante: " + cantidad);
+    } else {
+        System.out.println("No hay stock disponible para: " + nombre);
+    }
+    
+    }
+
+
 }
