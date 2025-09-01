162ab7ae3670f79b5f3daf3064cac021018f726e
AlejaRivera11 <ariveram@estudiante.uniajc.edu.co>
2025-09-01 16:43:24 -0500

    Se agrega codigo a la clase Maquina: Pendiente hacer correcciones

--- src/Maquina.java
+++ src/Maquina.java
@@ -1,12 +1,39 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class Maquina {
+
     private double dineroRecibido;
-    
+    private Map<String,Producto> inventario = new HashMap<>();  
     
     public Maquina (double dineroRecibido){
-
+      this.dineroRecibido = dineroRecibido;
   }
+
+  public double getDineroRecibido(){
+    return dineroRecibido;
+  }
+
+  public void setDineroRecibido (double dineroRecibido){
+  this.dineroRecibido = dineroRecibido;
+  }
+
+  public void cargarProducto (){
+  }
+
+  public void mostrarProducto(){
+  }
+
+  public void seleccionarProducto(){
+  }
+
+  public void recibirDinero (double cantidad){
+    this.dineroRecibido += cantidad;
+    System.out.println("El dinero recibido es: $ " + this.dineroRecibido);
+    
+  }
+
+  
 
 }
 
